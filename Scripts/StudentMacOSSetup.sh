210d5dfcd253fe207afdac8f05d50ab298cf3b69
James Dearness <jdearness@gmail.com>
2024-10-28 15:33:17 +1100

    run as sudo

--- Scripts/StudentMacOSSetup.sh
+++ Scripts/StudentMacOSSetup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# This script flags that the Baseline setup has completed.  Deleting this fuile will cause Baseline Setup to rerun.
+# This script flags that the Baseline setup has completed.  Deleting this file will cause Baseline Setup to rerun.
 # Define the log file path
 log_file="$HOME/Library/Logs/BaselineSetupComplete.txt"
 
 # Create the file with the current time
-date +"%Y-%m-%d %H:%M:%S" > "$log_file"
+date +"%Y-%m-%d %H:%M:%S" > | sudo tee "$log_file" > /dev/null
 
 # Print success message
 echo "SetupComplete.txt created with the current time."
