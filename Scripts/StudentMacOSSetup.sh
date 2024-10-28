e2a2f4447a51a280d6ce1ecfaa3694d2f84867b2
James Dearness <jdearness@gmail.com>
2024-10-29 09:44:42 +1100

    Undo change to log path

--- Scripts/StudentMacOSSetup.sh
+++ Scripts/StudentMacOSSetup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script flags that the Baseline setup has completed.  Deleting this file will cause Baseline Setup to rerun.
 # Define the log file path
-log_file="/var/log/BaselineSetupComplete.txt"
+log_file="$HOME/Library/Logs/BaselineSetupComplete.txt"
 
 # Create the file with the current time
-date +"%Y-%m-%d %H:%M:%S" > | sudo tee "$log_file" > /dev/null
+date +"%Y-%m-%d %H:%M:%S" > "$log_file"
 
 # Print success message
 echo "SetupComplete.txt created with the current time."
