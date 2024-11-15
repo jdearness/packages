703d54684791d00481ca239709f9d86733fae1d4
James Dearness <jdearness@gmail.com>
2024-11-15 12:47:49 +1100

    Added hide local admin script

---
+++ Scripts/hideAdminAccount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Hide account from Users list
+dscl . create /Users/localadmin IsHidden 1
+# Hide home directory
+chflags hidden /Users/localadmin
+# Remove public folders
+dscl 
+delete Local/Defaults/SharePoints/Hidden\ User’s\ Public\ Folder/
+exit
+
+
