9b9f38ee79220655bd0c85ba743643aef6620db8
James Dearness <jdearness@gmail.com>
2024-11-16 07:08:03 +1100

    Adjusted management account name

--- Scripts/hideAdminAccount.sh
+++ Scripts/hideAdminAccount.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Hide account from Users list
-dscl . create /Users/localadmin IsHidden 1
+dscl . create /Users/lapsadmin IsHidden 1
 # Hide home directory from GUI
-chflags hidden /Users/localadmin
-fi
+chflags hidden /Users/lapsadmin
